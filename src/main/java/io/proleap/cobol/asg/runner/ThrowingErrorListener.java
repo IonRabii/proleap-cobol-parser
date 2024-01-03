d1ef0c52bb9691595bec2ba7e3acd22fbe9338e7
Ion Rabii <ion.rabii@iongroup.com>
2024-01-03 18:11:44 +0200

    v4.2.0 (write logs instead of blocking exception when the source has errors)

--- src/main/java/io/proleap/cobol/asg/runner/ThrowingErrorListener.java
+++ src/main/java/io/proleap/cobol/asg/runner/ThrowingErrorListener.java
@@ -11,14 +11,16 @@ package io.proleap.cobol.asg.runner;
 import org.antlr.v4.runtime.BaseErrorListener;
 import org.antlr.v4.runtime.RecognitionException;
 import org.antlr.v4.runtime.Recognizer;
-
-import io.proleap.cobol.asg.exception.CobolParserException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ThrowingErrorListener extends BaseErrorListener {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
-	@Override
-	public void syntaxError(final Recognizer<?, ?> recognizer, final Object offendingSymbol, final int line,
-			final int charPositionInLine, final String msg, final RecognitionException e) {
-		throw new CobolParserException("syntax error in line " + line + ":" + charPositionInLine + " " + msg);
-	}
+    @Override
+    public void syntaxError(final Recognizer<?, ?> recognizer, final Object offendingSymbol, final int line,
+                            final int charPositionInLine, final String msg, final RecognitionException e) {
+        logger.debug("syntax error in line {}:{} {}", line, charPositionInLine, msg);
+//        throw new CobolParserException("syntax error in line " + line + ":" + charPositionInLine + " " + msg);
+    }
 }
